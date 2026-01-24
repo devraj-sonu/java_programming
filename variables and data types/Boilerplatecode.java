c2b721c3c2ea94fcc5f244b9579b6a94ea61565e
devraj-sonu <devraj6477@gmail.com>
2026-01-24 21:53:41 +0530

    Add modified variables and data type folder in git repo

---
+++ variables and data types/Boilerplatecode.java
@@ -0,0 +1,7 @@
+public class Boilerplatecode {
+    public static void main(String args[]) {
+
+    }
+}
+
+//boilerplate code of java
---
+++ variables and data types/printStar.java
@@ -0,0 +1,12 @@
+public class printStar {
+    public static void main(String args[]){
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
+
+
+//caution : it is not recommended to use this code there are more appropriate logic behind it.
