6c98b654d08329da9742b06b633e3aa60a8d0577
devraj-sonu <devraj6477@gmail.com>
2026-01-27 23:17:12 +0530

    Add some changes and completely uplad variables and data types folder in git repo

--- variables and data types/runJava.java
+++ variables and data types/runJava.java
@@ -1,3 +1,5 @@
 public class runJava {
-    //source code -[compiler] ->byte
+    //source code -[compiler] -> byte code.class -> [java vertual machine(JVM)] -> Native code
+    //source code to byte code transformation is called as compilation.
+    //byte code to native code transformation is called as a execution process.
 }
