d7c757177f69217a56e5b63c4bb1e0800414e183
devraj-sonu <devraj6477@gmail.com>
2026-01-28 23:55:38 +0530

    Add Operators folder in git repo

---
+++ Operators/ArithmeticOperator.java
@@ -0,0 +1,15 @@
+package Operators;
+
+import java.util.Scanner;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = 10;
+        int B = 20;
+
+        System.out.println("Add = " + (A+B));
+        System.out.println("Sub = " + (A-B));
+        sc.close();
+    }
+}
---
+++ Operators/types_of_operators.java
@@ -0,0 +1,7 @@
+//types of operators in java
+
+//arithmetic operators(binary/unary)
+//relation operators
+//logical operators
+//bitwise operators
+//assignment operators
