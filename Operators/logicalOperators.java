55bf1d5a312cd8c62796ca1b752e99df540b9b43
devraj-sonu <devraj6477@gmail.com>
2026-01-31 13:07:11 +0530

    Add some changes into operators folder

---
+++ Operators/logicalOperators.java
@@ -0,0 +1,7 @@
+package Operators;
+
+public class logicalOperators {
+    public static void main(String[] args) {
+        System.out.println((3<2) && (5>0));
+    }
+}
---
+++ Operators/relationalOperator.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class relationalOperator {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        System.out.println((A==B));
+
+    }
+}
