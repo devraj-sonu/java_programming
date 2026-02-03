899e98531bceed255af22fb56ff820cf0d65e791
devraj-sonu <devraj6477@gmail.com>
2026-02-03 23:05:24 +0530

    Add conditionalstatements folder in git repo

---
+++ conditionalstatements/ifElse.java
@@ -0,0 +1,15 @@
+package conditionalstatements;
+
+public class ifElse {
+    public static void main(String[] args) {
+        int age = 22;
+        if(age>=10) {
+        System.out.println("adult, Can vote, can drive ");
+        }
+        else{
+        System.out.println("Not adult");
+        }
+
+    }
+    
+}
---
+++ conditionalstatements/largestTwonum.java
@@ -0,0 +1,14 @@
+package conditionalstatements;
+
+public class largestTwonum {
+    public static void main(String[] args) {
+        int A = 1;
+        int B = 5;
+
+        if(A > B) {
+            System.out.println("A is greater of two numbers");
+        } else {
+            System.out.println("B is greater than two numbers");
+        }
+    }
+}
