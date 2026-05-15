31408a4d06a5e1775a6a055cc615423811dc253d
devraj-sonu <devraj6477@gmail.com>
2026-05-15 10:37:22 +0530

    add some changes in file

---
+++ strings/stringBuilder.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class stringBuilder {
+    public static void main(String[]args) {
+        StringBuilder sb = new StringBuilder("");
+        for(char ch='a'; ch<='z'; ch++) {
+            sb.append(ch);
+        }//abcdefghijklmnop ....
+        //O(n)
+        //O(n^2)
+        System.out.println(sb);
+    }
+}
